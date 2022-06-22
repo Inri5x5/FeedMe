9b9c6da2c580fbbc295d95f79bf34dc6a5093657
Nadya <nadyausitorus@gmail.com>
2022-06-22 23:19:27 +1000

    Basic setup login

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,53 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
+ 
+mysql = MySQL(app)
+
 
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return "Login via the form"
+     
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        cursor = mysql.connection.cursor()
+
+        cursor.execute('''SELECT 1 FROM MyTable WHERE email = %s''', email)
+        info = cursor.fetchone()
+        if not info:
+            mysql.connection.commit()
+            cursor.close()
+            return f"Email doesn't exist"
+
+        cursor.execute('''SELECT 1 FROM MyTable WHERE email = %s, password = %s''', 
+                        (email, password))
+        info = cursor.fetchone()
+        if not info:
+            mysql.connection.commit()
+            cursor.close()
+            return f"Wrong password"
+
+        mysql.connection.commit()
+        cursor.close()
+        return f"User logged in"
+
+
+app.run(host='localhost', port=5000)
---
+++ backend/auth.py
@@ -0,0 +1,19 @@
+'''
+auth.py
+login and logout as a registered user
+'''
+
+from distutils.log import error
+import re
+
+def login(email, password):
+
+    return 0
+
+def logout(email):
+
+    return 0
+    
+
+    
+
