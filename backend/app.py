7d7cfe70e2813b1ee355e798e3fe91609f96e00e
Nadya <nadyausitorus@gmail.com>
2022-06-25 17:04:41 +1000

    Edit login function

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,18 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
+from helper import create_token
+import json
+import jwt
 
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flask'
+app.config['MYSQL_DB'] = 'feedme_test'
  
 mysql = MySQL(app)
 
-
-@app.route("/")
-def home():
-    return "Hello, Flask!"
 
 @app.route('/form')
 def form():
@@ -25,29 +24,50 @@ def login():
         return "Login via the form"
      
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+        email = request.json['email']
+        password = request.json['password']
 
         cursor = mysql.connection.cursor()
 
         cursor.execute('''SELECT 1 FROM MyTable WHERE email = %s''', email)
         info = cursor.fetchone()
         if not info:
-            mysql.connection.commit()
             cursor.close()
-            return f"Email doesn't exist"
+            return {
+                "status": 400,
+                "body": {"error": "Email is not registered"}
+            }
 
         cursor.execute('''SELECT 1 FROM MyTable WHERE email = %s, password = %s''', 
                         (email, password))
         info = cursor.fetchone()
         if not info:
-            mysql.connection.commit()
             cursor.close()
-            return f"Wrong password"
+            return {
+                "status": 400,
+                "body": {"error": "Wrong password"}
+            }
 
-        mysql.connection.commit()
+        # check if contributor or regular user
+
         cursor.close()
-        return f"User logged in"
+        
+        token = create_token(email)
+
+        return {
+            "status": 200,
+            "body": {"token": token}
+        }
+
+@app.route('/logout', methods = ['GET'])
+def logout():
+
+    # delete token
+
+    return {
+        "status": 200
+    }
 
 
-app.run(host='localhost', port=5000)
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
