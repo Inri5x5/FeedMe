4210288502f45da8b1abc827d4fd2c13115ef720
Nadya <nadyausitorus@gmail.com>
2022-06-26 17:32:05 +1000

    Added helper functions get user and get contributor

--- backend/helper.py
+++ backend/helper.py
@@ -8,3 +8,49 @@ import jwt
 
 def create_token(email):
     return jwt.encode({"email": email}, "", algorithm="HS256")
+
+def get_contributor(email, password):
+    # Get contributors json file
+    f = open('contributors.json')
+    contributors = json.load(f)
+    f.close()
+
+    ret = {}
+    for c in contributors:
+        # Contributor exists
+        if c["email"] == email & c["password"] == password:
+            ret = {
+                "status": 200,
+                "body": {}
+            }
+        # Contributor exists but wrong password
+        elif c["email"] == email & c["password"] != password:
+            ret = {
+                "status": 400,
+                "body": {"error": "Wrong password"}
+            }
+
+    return ret
+
+def get_ruser(email, password):
+    # Get users json file
+    f = open('users.json')
+    users = json.load(f)
+    f.close()
+
+    ret = {}
+    for u in users:
+        # User exists
+        if u["email"] == email & u["password"] == password:
+            ret = {
+                "status": 200,
+                "body": {}
+            }
+        # User exists but wrong password
+        elif u["email"] == email & u["password"] != password:
+            ret = {
+                "status": 400,
+                "body": {"error": "Wrong password"}
+            }
+
+    return ret
