a278fa4a1349b4eec10864f640b59f2f2287c652
Kaylee Jo Vuong <kaylee.j.vuong@gmail.com>
2022-06-26 20:39:54 +1000

    added new route and fixed case sensitivity

--- backend/json_app.py
+++ backend/json_app.py
@@ -10,6 +10,10 @@ def form1():
 @app.route('/search/ingredients')
 def form2():
     return render_template('ingredients.html')
+
+@app.route('/search/category/ingredients')
+def form3():
+    return render_template('ingredient_category.html')
  
 @app.route('/categories', methods = ['GET'])
 def categories():
@@ -23,7 +27,7 @@ def categories():
         categories.append(dict["name"])
 
     print(categories) 
-    
+
     # Format return dict
     ret = {"status": 200,
             "body": {"categories": categories}}
@@ -42,13 +46,44 @@ def ingredients():
     # Search ingredient dict for string matches
     suggestions = []
     for dict in data:
-        if ingredient in dict["name"]:
+        if ingredient.lower() in dict["name"].lower():
             suggestions.append(dict["name"])
 
     # Format return dict
     ret = {"status": 200,
             "body": {"suggestions": suggestions}}
 
+    return ret
+
+@app.route('/category/ingredients', methods = ['GET'])
+def category_ingredients():
+    # Get user input (string)
+    category = request.args['Category']
+
+    # Load category_id to category name mapping and get id
+    i = open('data/ingredient_categories_table.json')
+    category_data = json.load(i)
+    category_id = 0
+    for j in category_data:
+        if category == j["name"]:
+            category_id = j["category_id"]
+
+    # Load ingredient data and get all ingredient in category
+    f = open('data/ingredients_table.json')
+    ingredient_data = json.load(f)
+    ingredients = []
+    for dict in ingredient_data:
+        if category_id == dict["ingredient_category_id"]:
+            ingredients.append(dict["name"])
+
+    # Format return dict
+    if len(ingredients) == 0:
+        ret = {"status": 400,
+                "body": {"error": "Invalid category name"}}
+    else:
+        ret = {"status": 200,
+                "body": {"ingredients": ingredients}}
+    
     return ret
 
  
