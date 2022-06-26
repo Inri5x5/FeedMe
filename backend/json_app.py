59a7e216555ed3d40cbfbd7ea58d9d15fb003573
Nadya <nadyausitorus@gmail.com>
2022-06-26 19:35:50 +1000

    Updated login and logout according to the new json files

--- backend/json_app.py
+++ backend/json_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from helper import create_token
+from helper import get_contributor, get_ruser, check_password, generate_token
 import json
 
 app = Flask(__name__)
@@ -12,41 +12,47 @@ def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
+        user_id = -1
+        is_contributor = False
 
-        # Get users json file
-        f = open('backend/data/users.json')
-        users = json.load(f)
-        f.close()
-
-        user_exists = 0
-        for user in users:
-            # User exists
-            if user["email"] == email and user["password"] == password:
-                user_exists = 1
-                is_contributor = user["is_contributor"]
-            # User exists but wrong password
-            elif user["email"] == email and user["password"] != password:
-                return {
-                    "status": 400,
-                    "body": {"error": "Wrong password"}
-                }
-        
-        # User does not exist
-        if not user_exists:
+        # Check user type
+        contributor = get_contributor(email)
+        ruser = get_ruser(email)
+        if (contributor < 0 and ruser < 0): # User does not exist
             return {
                 "status": 400,
                 "body": {"error": "Email is not registered"}
             }
-            
+        elif (contributor >= 0):            # User is a contributor
+            user_id = contributor
+            is_contributor = True
+        elif (ruser >= 0):                  # User is a regular user
+            user_id = ruser
+            is_contributor = False
+
+        # Check password
+        if not check_password(email, password, is_contributor):
+            return {
+                "status": 400,
+                "body": {"error": "Wrong password"}
+            }
+        
         # Get tokens json file
-        f = open('backend/data/tokens.json', 'r')
+        f = open('backend/data/tokens_table.json', 'r')
         tokens = json.load(f)
         f.close()
         
-        # Create token and update tokens json file
-        token = create_token(email)
-        tokens.append({email: token})
-        f = open('backend/data/tokens.json', 'w')
+        # Create token 
+        token = generate_token(email)
+        print(token)
+
+        # Update tokens json file
+        tokens.append({
+            "token": token,
+            "user_id": user_id,
+            "is_contributor": is_contributor
+            })
+        f = open('backend/data/tokens_table.json', 'w')
         f.write(json.dumps(tokens))
         f.close()
 
@@ -57,25 +63,28 @@ def login():
 
 @app.route('/logout', methods = ['GET'])
 def logout():
-    email = request.form.get('email')
     token = request.form.get('token')
-    
+
     # Get tokens json file
-    f = open('backend/data/tokens.json', 'r')
+    f = open('backend/data/tokens_table.json', 'r')
     tokens = json.load(f)
     f.close()
 
     # Validate token
+    token_exists = False
     for t in tokens:
-        if email in t and t[email] != token:
-            return {
-                "status": 400,
-                "body": {"error": "Invalid token"}
-            }
+        if t["token"] == token:
+            token_exists = True
+            
+    if not token_exists:
+        return {
+            "status": 400,
+            "body": {"error": "Invalid token"}
+        }
         
     # Delete token from tokens json file
-    tokens = [i for i in tokens if not (i[email] == token)]
-    f = open('tokens.json', 'w+')
+    tokens = [i for i in tokens if not (i["token"] == token)]
+    f = open('backend/data/tokens_table.json', 'w+')
     f.write(json.dumps(tokens))
     f.close()
 
