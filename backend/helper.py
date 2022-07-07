e52f4700b75f3cd1238f5f8f65146f026443e661
Nadya <nadyausitorus@gmail.com>
2022-07-07 13:00:50 +1000

    Changed json to sql

--- backend/helper.py
+++ backend/helper.py
@@ -5,26 +5,34 @@ Helper functions
 import json
 import jwt
 import re
+import sqlite3
 from datetime import datetime
 
 regex = '^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$'
+
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect('feedme.sqlite')
+    except sqlite3.error as e:
+        print(e)
+    return conn
 
 def generate_token(email):
     payload = {"email": email, "datetime": datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")}
     return jwt.encode(payload, "", algorithm="HS256")
 
 def validate_token(token):
-    # Get tokens json file
-    f = open('./data/tokens_table.json', 'r')
-    tokens = json.load(f)
-    f.close()
+    conn = db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT 1 FROM Tokens WHERE token = %s', [token])
+    info = cur.fetchone()
+    cur.close()
 
-    # Validate token
-    for t in tokens:
-        if t["token"] == token:
-            return True
-            
-    return False
+    if not info:
+        return False
+    else:
+        return True
 
 def valid_email(email):
     if not re.search(regex, email):
@@ -33,50 +41,42 @@ def valid_email(email):
         return True
 
 def get_contributor(email):
-    # Get contributors json file
-    f = open('./data/contributors_table.json')
-    contributors = json.load(f)
-    f.close()
+    conn = db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT contributor_id FROM Contributors WHERE email = %s', [email])
+    info = cur.fetchone()
+    cur.close()
 
-    user_id = -1
-    for c in contributors:
-        # Contributor exists
-        if c["email"] == email:
-            user_id = c["contributor_id"]
-
-    return user_id
+    if not info: 
+        return -1
+    else: 
+        return info
 
 def get_ruser(email):
-    # Get users json file
-    f = open('./data/rusers_table.json')
-    users = json.load(f)
-    f.close()
+    conn = db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT ruser_id FROM RUsers WHERE email = %s', [email])
+    info = cur.fetchone()
+    cur.close()
 
-    user_id = -1
-    for u in users:
-        # User exists
-        if u["email"] == email:
-            user_id = u["ruser_id"]
-
-    return user_id
+    if not info: 
+        return -1
+    else: 
+        return info
 
 def check_password(email, password, is_contributor):
+    conn = db_connection()
+    cur = conn.cursor()
+    
     if (is_contributor):
-        f = open('./data/contributors_table.json')
-        contributors = json.load(f)
-        f.close()
-
-        for c in contributors:
-            if c["email"] == email and c["password"] == password:
-                return True
+        cur.execute('SELECT * from Contributors WHERE email = %s AND password = %s')
+        info = cur.fetchone()
     else:
-        f = open('./data/rusers_table.json')
-        users = json.load(f)
-        f.close()
+        cur.execute('SELECT * from Rusers WHERE email = %s AND password = %s')
+        info = cur.fetchone()
 
-        for u in users:
-            if u["email"] == email and u["password"] == password:
-                return True
-
-    return False
+    if not info: 
+        return False
+    else:
+        return True
 
