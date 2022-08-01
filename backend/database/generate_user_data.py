afad5458a8cb30e6fad29f84d6bf79a73378df90
Angeline Parrella <angelineparrella@Angelines-MacBook-Air.local>
2022-08-01 14:18:22 +1000

    adding recipes to public so that they show up in search

--- backend/database/generate_user_data.py
+++ backend/database/generate_user_data.py
@@ -34,24 +34,27 @@ conn.commit()
 # insert into personalRecipes
 delete_query = """DELETE FROM personalRecipes WHERE 1"""
 cursor.execute(delete_query)
-insert_into_personalRecipes_query = """
-INSERT INTO 
-    personalRecipes (ruser_id, contributor_id, recipe_id)
-VALUES
-    (null, 0, 0),
-    (null, 0, 1),
-    (null, 0, 2),
-    (null, 0, 3),
-    (null, 0, 4),
-    (null, 0, 5),
-    (null, 0, 6),
-    (null, 0, 7),
-    (null, 0, 8),
-    (null, 0, 9)
-"""
+# insert_into_personalRecipes_query = """
+# INSERT INTO 
+#     personalRecipes (ruser_id, contributor_id, recipe_id)
+# VALUES
 
-cursor = cursor.execute(insert_into_personalRecipes_query)
-conn.commit()
+# """
+# """
+# INSERT INTO 
+#     PersonalRecipes (recipe_id, contributor_id)
+# VALUES
+#     (?, 0)
+# """
+# i = 0
+# for i in [0, 153]:
+#     if i < 90 or i > 110:
+#         cursor = cursor.execute(insert_into_PublicRecipes_query, (i))
+#         conn.commit()
+
+
+# cursor = cursor.execute(insert_into_personalRecipes_query)
+# conn.commit()
 
 # insert into publicRecipes
 delete_query = """DELETE FROM PublicRecipes WHERE 1"""
@@ -60,20 +63,29 @@ insert_into_PublicRecipes_query = """
 INSERT INTO 
     PublicRecipes (recipe_id, contributor_id)
 VALUES
-    (0, 0),
-    (1, 0),
-    (2, 0),
-    (3, 0),
-    (4, 0),
-    (5, 0),
-    (6, 0),
-    (7, 0),
-    (8, 0),
-    (9, 0)
+    (?, ?)
 """
+i = 0
+for i in range(0, 152):
+    if i < 90 or i > 110:
+        cursor = cursor.execute(insert_into_PublicRecipes_query, (i, 0))
+        conn.commit()
 
-cursor = cursor.execute(insert_into_PublicRecipes_query)
-conn.commit()
+
+# insert_query = """INSERT INTO recipes (id, title, description, image, video, time_required, servings) VALUES (?, ?, ?, ?, ?, ?, ?)"""
+# for recipe in recipe_data:
+#     cursor.execute('SELECT max(id) from recipes')
+#     new_id = cursor.fetchone()
+#     #print("THE ID IS HERE!!! " + new_id[0])
+#     if new_id[0] is None:
+#         new_id = 0
+#     else:
+#         new_id = new_id[0] + 1
+#     cursor = cursor.execute(insert_query, (new_id, recipe['name'], recipe['description'], recipe['image'], recipe['video'], recipe['time'], recipe['servings']))
+#     conn.commit()
+
+# cursor = cursor.execute(insert_into_PublicRecipes_query)
+# conn.commit()
 
 # insert into recipeRatings
 delete_query = """DELETE FROM recipeRatings WHERE 1"""
